8c3c808e7811cb8006f7e75c386814ae11e20a9a
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2016-09-10 17:27:43 +0100

    MongoDB port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const
     mongoose = require("mongoose");
 
 var app = express();
-mongoose.connect("mongodb://gpereira.tk/byll");
+mongoose.connect("mongodb://gpereira.tk:27017/byll");
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({verify: verifyRequestSignature}));
