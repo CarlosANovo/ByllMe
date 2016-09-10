5e2f471cbd694db73cad9bc9a591e918826635f9
CarlosANovo <casc.novo@gmail.com>
2016-09-10 20:15:38 +0100

    port change :shit:

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const
 
 var app = express();
 /*mongoose.connect("mongodb://localhost/byll");*/
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({verify: verifyRequestSignature}));
 app.use(express.static('public'));
